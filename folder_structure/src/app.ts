3bcf44f766bf164abc395a1f8cfd476a1ab8c7af
Akash2811000 <akashgupta7383@gmail.com>
2022-07-05 11:49:03 +0530

    node ts

--- folder_structure/src/app.ts
+++ folder_structure/src/app.ts
@@ -1,14 +1,21 @@
 import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 import mongoose from 'mongoose';
-const connection = mongoose.connect('mongodb://localhost:27017/mydata');
+const connection = mongoose.connect('mongodb://localhost:27017/mytsdata');
+import {router} from './controller/student_controller'
 
+
+app.use(express.json());
+app.use('/students',router);
 app.get('/', (req: Request, res: Response) => {
 
 res.send('Express + TypeScript Server');
 res.end();
 
 });
+
+
+
 
 app.listen(3000, () => {
 
--- folder_structure/src/model/students.ts
+++ folder_structure/src/model/students.ts
@@ -1,24 +1,24 @@
-import mongoose from "mongoose";
 
-
-
-interface StudentI {
-    "StudentId": Number,
-    "Name": String,
-    "Address": String,
-    "Fees": {
-        "Amount": Number,
-        "PaymentDate": Date,
-        "Status": String
+import mongoose from "mongoose"; 
+const StudentSchma = new mongoose.Schema({
+    StudentId: String,
+    Name: String,
+    Address: String,
+    Fees: {
+        Amount: Number,
+        PaymentDate: Date,
+        Status: String
     },
-    "Result": {
-        "Hindi": Number,
-        "Eng": Number,
-        "Math": Number,
-        "Total": Number,
-        "Grade": String
+    Result: {
+        Hindi: Number,
+        Eng: Number,
+        Math: Number,
+        Total: Number,
+        Grade: String
     }
 
-}
+})
 
+const studentmodel = mongoose.model('students',StudentSchma);
 
+export {studentmodel};
